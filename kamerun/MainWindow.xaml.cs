0691a9f4246d192fb1f9fa7636b77d7ba9f0d34f
keisei1305 <kippop88@gmail.com>
2023-10-01 12:43:11 +0300

    s

--- kamerun/MainWindow.xaml.cs
+++ kamerun/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Linq;
+using System.Collections.Generic;
 using SharpGL;
 
 namespace kamerun
@@ -19,6 +21,9 @@ namespace kamerun
             rotate = new Point3d(0f, 0f, 0f);
         }
 
+        #region Задачи
+
+        #region Задание 1
         private void SenegaleBuild()
         {
             gl.Begin(OpenGL.GL_QUADS);
@@ -58,41 +63,16 @@ namespace kamerun
             gl.Vertex(0.5f, -0.8f);
             gl.End();
         }
+        #endregion
 
-        private void Parallelepiped(double x, double y, double z, double dx, double dy, double dz)
-        {
-            gl.Begin(OpenGL.GL_QUADS);
-            gl.Vertex(x+dx, y+dy, z+dz);
-            gl.Vertex(x+dx, y+dy, z);
-            gl.Vertex(x, y+dy, z);
-            gl.Vertex(x, y + dy, z+dz);
-            gl.End();
-
-            gl.Begin(OpenGL.GL_QUADS);
-            gl.Vertex(x + dx, y, z + dz);
-            gl.Vertex(x + dx, y, z);
-            gl.Vertex(x, y, z);
-            gl.Vertex(x, y, z + dz);
-            gl.End();
-
-            gl.Begin(OpenGL.GL_QUAD_STRIP);
-            gl.Vertex(x + dx, y, z + dz);
-            gl.Vertex(x + dx, y + dy, z + dz);
-            gl.Vertex(x + dx, y, z);
-            gl.Vertex(x + dx, y + dy, z);
-            gl.Vertex(x, y, z);
-            gl.Vertex(x, y + dy, z);
-            gl.Vertex(x, y, z + dz);
-            gl.Vertex(x, y + dy, z + dz);
-            gl.End();
-        }
+        #region Задание 2        
 
         #region Всё для танков
         private void Bashnya()
         {
             //x=-1f,y=1f,z=-1f dx=2f, dy = 2f, dz=2f
             gl.Color(0f, 0.4f, 0f);
-            Parallelepiped(-1f,1f,-1f,2f,2f,2f);
+            Parallelepiped(-1f, 1f, -1f, 2f, 2f, 2f);
         }
 
         private void Corpus()
@@ -112,33 +92,6 @@ namespace kamerun
 
         private void gusenica1()
         {
-            gl.Begin(OpenGL.GL_QUADS);
-            gl.Color(0f, 0f, 0f);
-            gl.Vertex(2.5f, -0.8f, 1.8f);
-            gl.Vertex(2.5f, -0.8f, 1.6f);
-            gl.Vertex(-2.5f, -0.8f, 1.6f);
-            gl.Vertex(-2.5f, -0.8f, 1.8f);
-            gl.End();
-
-            gl.Begin(OpenGL.GL_QUADS);
-            gl.Color(0f, 0f, 0f);
-            gl.Vertex(2.3f, -1.6f, 1.8f);
-            gl.Vertex(2.3f, -1.6f, 1.6f);
-            gl.Vertex(-2.3f, -1.6f, 1.6f);
-            gl.Vertex(-2.3f, -1.6f, 1.8f);
-            gl.End();
-
-            gl.Begin(OpenGL.GL_QUAD_STRIP);
-            gl.Color(0f, 0f, 0f);
-            gl.Vertex(2.5f, -0.8f, 1.8f);
-            gl.Vertex(2.3f, -1.6f, 1.8f);
-            gl.Vertex(2.5f, -0.8f, 1.6f);
-            gl.Vertex(2.3f, -1.6f, 1.6f);
-            gl.Vertex(-2.5f, -0.8f, 1.6f);
-            gl.Vertex(-2.3f, -1.6f, 1.6f);
-            gl.Vertex(-2.5f, -0.8f, 1.8f);
-            gl.Vertex(-2.3f, -1.6f, 1.8f);
-            gl.End();
         }
 
         private void gusenica2()
@@ -182,37 +135,189 @@ namespace kamerun
             gusenica2();
         }
 
+        #endregion
+
+        #endregion
+
+        #region Вспомогательные инструменты для рисования
+        private void Parallelepiped(double x, double y, double z, double dx, double dy, double dz)
+        {
+            gl.Begin(OpenGL.GL_QUADS);
+            gl.Vertex(x+dx, y+dy, z+dz);
+            gl.Vertex(x+dx, y+dy, z);
+            gl.Vertex(x, y+dy, z);
+            gl.Vertex(x, y + dy, z+dz);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_QUADS);
+            gl.Vertex(x + dx, y, z + dz);
+            gl.Vertex(x + dx, y, z);
+            gl.Vertex(x, y, z);
+            gl.Vertex(x, y, z + dz);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_QUAD_STRIP);
+            gl.Vertex(x + dx, y, z + dz);
+            gl.Vertex(x + dx, y + dy, z + dz);
+            gl.Vertex(x + dx, y, z);
+            gl.Vertex(x + dx, y + dy, z);
+            gl.Vertex(x, y, z);
+            gl.Vertex(x, y + dy, z);
+            gl.Vertex(x, y, z + dz);
+            gl.Vertex(x, y + dy, z + dz);
+            gl.End();
+        }
+
+        private void Polygon(params Point3d[] points)
+        {
+            gl.Begin(OpenGL.GL_QUAD_STRIP);
+            for (int i = 0; i < points.Length; i++)
+            {
+                gl.Vertex(points[i].x, points[i].y, points[i].z);
+            }
+            gl.Vertex(points[0].x, points[0].y, points[0].z);
+            gl.Vertex(points[1].x, points[1].y, points[1].z);
+            gl.End();
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            for (int i = 0; i < points.Length; i += 2)
+            {
+                gl.Vertex(points[i].x, points[i].y, points[i].z);
+            }
+            gl.End();
+
+            gl.Begin(OpenGL.GL_POLYGON);
+            for (int i = 1; i < points.Length; i += 2)
+            {
+                gl.Vertex(points[i].x, points[i].y, points[i].z);
+            }
+            gl.End();
+        }
+
+        private void Polygon(RGB[] rgb, params Point3d[] points)
+        {
+            gl.Color(rgb[0].rgb); 
+            gl.Begin(OpenGL.GL_QUAD_STRIP);
+            for (int i = 0; i < points.Length; i++)
+            {
+                gl.Vertex(points[i].x, points[i].y, points[i].z);
+            }
+            gl.Vertex(points[0].x, points[0].y, points[0].z);
+            gl.Vertex(points[1].x, points[1].y, points[1].z);
+            gl.End();
+
+            gl.Color(rgb[1].rgb);
+            gl.Begin(OpenGL.GL_POLYGON);
+            for (int i = 0; i < points.Length; i += 2)
+            {
+                gl.Vertex(points[i].x, points[i].y, points[i].z);
+            }
+            gl.End();
+
+            gl.Color(rgb[2].rgb);
+            gl.Begin(OpenGL.GL_POLYGON);
+            for (int i = 1; i < points.Length; i += 2)
+            {
+                gl.Vertex(points[i].x, points[i].y, points[i].z);
+            }
+            gl.End();
+        }
+
+        private void Prism(float dz, params Point3d[] points)
+        {
+            int n = points.Length;
+            List<Point3d> newPoints = new List<Point3d>(n*2);
+            for(int i=0; i<n; i++)
+            {
+                newPoints.Add(points[i]);
+                newPoints.Add(new Point3d(points[i].x, points[i].y, points[i].z+dz));
+            }
+            Polygon(newPoints.ToArray());
+        }
+
+        private void Prism(RGB[] rgb, float dz, params Point3d[] points)
+        {
+            int n = points.Length;
+            List<Point3d> newPoints = new List<Point3d>(n * 2);
+            for (int i = 0; i < n; i++)
+            {
+                newPoints.Add(points[i]);
+                newPoints.Add(new Point3d(points[i].x, points[i].y, points[i].z + dz));
+            }
+            Polygon(rgb,newPoints.ToArray());
+        }
+
+        struct Point3d
+        {
+            public float x, y, z;
+            public Point3d(float x, float y, float z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+        struct RGB
+        {
+            public float[] rgb;
+            public RGB(float R, float G, float B)
+            {
+                rgb = new float[3];
+                rgb[0] = R;
+                rgb[1]= G;
+                rgb[2] = B;
+            }
+
+            public RGB(int R, int G, int B)
+            {
+                rgb = new float[3];
+                rgb[0]= R / 255;
+                rgb[1] = G / 255;
+                rgb[1] = B / 255;
+            }
+        }
+        #endregion
+
+        #region examples
+        private void symbolM()
+        {
+            Prism(new RGB[3] {new RGB(1f,0f,0f), new RGB(0f,1f,0f), new RGB(0f,0f,1f)}, 2f, new Point3d(-2f, -2f, -1f),
+                new Point3d(-1f, 1f, -1f),
+                new Point3d(0f, 0f, -1f),
+                new Point3d(1f, 1f, -1f),
+                new Point3d(2f, -2f, -1f));
+        }
+        #endregion
+
+        #region openglControlInit
         private void OpenGLControl_OpenGLDraw(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
         {
-            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
-            TankBuild();
-            gl.Flush();
+            Draw();
         }
 
         private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
         {
             gl.ClearColor(1f, 1f, 1f, 1f);
             gl.Scale(scale.x,scale.y,scale.z);
+            Draw();
         }
 
         private void OpenGLControl_Resized(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
         {
 
         }
-
+        #endregion
         private void openGLControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            gl.Rotate(4f, 4f, 0f);
+            gl.Rotate(15f, 0f, 0f);
+            openGLControl.DoRender();
+            Draw();
         }
-    }
-    struct Point3d
-    {
-        public float x, y, z;
-        public Point3d(float x, float y, float z)
+
+        private void Draw()
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            symbolM();
         }
     }
 }
