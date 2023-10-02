b5ffac7fc943e943bea8510e13b2443f04e68053
keisei1305 <kippop88@gmail.com>
2023-10-02 14:47:51 +0300

    Create Infastructure and Point3d

---
+++ kamerun/Infrastructure/Point3d.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamerun.Infrastruct
+{
+    internal class Point3d
+    {
+        public float x, y, z;
+        public Point3d(float x,float y,float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Point3d (Point3d point)
+        {
+            this.x = point.x;
+            this.y = point.y;
+            this.z = point.z;
+        }
+    }
+}
