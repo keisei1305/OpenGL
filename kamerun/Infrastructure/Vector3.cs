8cb775e92b1e7242e2dbac7e4e343ce4c22e9bed
keisei1305 <kippop88@gmail.com>
2023-10-02 14:49:31 +0300

    Fix Point3d to Vector3

---
+++ kamerun/Infrastructure/Vector3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamerun.Infrastructure
+{
+    class Vector3
+    {
+        public float x,y,z;
+        public Vector3()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
