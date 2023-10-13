890915b7b6a62a5f62656ac9630452199caf55d8
keisei1305 <kippop88@gmail.com>
2023-10-14 00:30:21 +0300

    Добавлен класс вектор

---
+++ kamerun/Infrastructure/Vector2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamerun.Infrastructure
+{
+    class Vector2
+    {
+        public float x,y;
+
+        public Vector2()
+        {
+            this.y = 0;
+            this.x = 0;
+        }
+
+        public Vector2(Vector2 vector2)
+        {
+            this.x = vector2.x;
+            this.y = vector2.y;
+        }
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+         public static Vector2 operator /(Vector2 vector, float digit)
+        {
+            return new Vector2(vector.x / digit, vector.y / digit);
+        }
+    }
+}
